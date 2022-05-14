b6c1f4cdc9ec417e1be29c759c32cba73d7afa78
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-05-13 20:38:51 -0600

    built a new js file for questions

---
+++ questions.js
@@ -0,0 +1,40 @@
+const questions = [
+{
+    question: "What are variables used for in JavaScript Programs?",
+    correct: "Storing numbers, dates, or other values",
+    answers: [
+        "Storing numbers, dates, or other values",
+        "Varying randomly",
+        "Causing high-school algebra flashbacks",
+        "None of the above",
+    ],
+},
+
+{
+    question: "Which types of image maps can be used with JavaScript?",
+    correct: "Client-side image maps",
+    answers: [
+        "Server-side image maps",
+        "Server-side image maps and Client-side image maps",
+        "Client-side image maps",
+        "None of the above",
+    ],
+},
+
+{
+    question: "Arrays in JavaScript are defined by which of the following statements?",
+    correct: "It is an ordered list of values",
+    answers: [
+        "It is an ordered list of objects",
+        "It is an ordered list of string",
+        "It is an ordered list of functions",
+        "It is an ordered list of values",
+    ],
+},
+
+{
+    question:"Which of the following can be used to call a JavaScript Code Snippet?",
+    correct: "Function/Method",
+    answers: ["Function/Method", "Preprocessor", "Triggering Event", "RMI"],
+},
+];
--- script.js
+++ script.js
@@ -5,38 +5,75 @@ const questionContainerEl = document.getElementById("question-container");
 const questionEl = document.getElementById("question");
 const answerbtnsEl = document.getElementById("answer-buttons");
 
+var timerText = document.querySelector(".timer-text");
+var score = 0;
 let mixQuestions, currentQIndex;
 
-startButton.addEventListener("click", startGame);
 
-function startGame() {
-  console.log("started");
-  startButton.classList.add("hide");
-  mixQuestions = questions.sort(() => Math.random() - 0.5);
-  currentQuestionIndex = 0;
-  questionContainerEl.classList.remove("hide");
-  setNextQuestion();
+startButton.addEventListener("click", startGame);
+startTimer();
+
+
+function startTimer() {
+  console.log("testing timer");
+  var gameTimer = setInterval(function () {
+    if (timer === 0) {
+      clearInterval(gameTimer);
+      endGame();
+    }
+
+    timerText.textContent = "Time Remaining: " + timer;
+    timer--;
+  }, 1000);
 }
 
+
+function startGame() {
+console.log("started");
+startButton.classList.add("hide");
+mixQuestions = questions.sort(() => Math.random() - .5);
+currentQIndex = 0;
+questionContainerEl.classList.remove("hide");
+setNextQuestion();
+}
+
+
+function setNextQuestion() {
+    resetCard()
+    showQuestion(mixQuestions[currentQIndex]);
+} 
+
 function showQuestion(question) {
-  questionEl.innerHTML = question.question;
-  question.answers.forEach((answer) => {
+    questionEl.innerHTML = question.question;
+    question.answers.forEach((answer) => {
     const button = document.createElement("button");
     button.innertext = answer.text;
     button.classList.add("btn");
     if (answer.correct) {
-      button.dataset.correct = answer.correct;
+        button.dataset.correct = answer.correct;
     }
     button.addEventListener("click", selectAnswer);
     answerbtnsEl.appendChild(button);
   });
 }
 
-function selectAnswer(event) {
-  const selectedButton = event.target;
-  const correct = selectedButton.dataset.correct;
+function resetCard() {
+    nextButton.classList.add("hide");
+    while (answerbtnsEl.firstChild) {
+        answerbtnsEl.removeChild
+        (answerbtnsEl.firstChild)
+    }
 }
 
-function setNextQuestion() {
-  showQuestion(mixQuestions[currentQIndex]);
+function selectAnswer(event) {
+  const selectedButton = event.target;
+
 }
+
+function endGame() {
+    
+}
+
+
+
+
